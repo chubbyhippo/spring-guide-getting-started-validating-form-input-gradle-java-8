40d1ae654fa652020a10b4f467ecdace0ad59436
ch <mkmsacc@outlook.com>
2020-11-09 20:32:32 +0700

    Add @Controller

--- src/main/java/com/example/validatingforminput/WebController.java
+++ src/main/java/com/example/validatingforminput/WebController.java
@@ -2,12 +2,14 @@ package com.example.validatingforminput;
 
 import javax.validation.Valid;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Controller
 public class WebController implements WebMvcConfigurer {
 
 	@Override
